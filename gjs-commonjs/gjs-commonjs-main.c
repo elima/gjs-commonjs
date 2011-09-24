513005666704c477f842c105f5437918998504d6
Eduardo Lima Mitev <elima@igalia.com>
2011-09-24 11:23:22 +0200

    Adds missing comment block to gjs-commonjs launcher program

--- gjs-commonjs/gjs-commonjs-main.c
+++ gjs-commonjs/gjs-commonjs-main.c
@@ -1,3 +1,25 @@
+/*
+ * gjs-commonjs-main.c
+ *
+ * gjs-commonjs, CommonJS support for the GNOME Javascript engine
+ *
+ * Copyright (C) 2011, Igalia S.L.
+ *
+ * Authors:
+ *   Eduardo Lima Mitev <elima@igalia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 3, or (at your option) any later version as published by
+ * the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License at http://www.gnu.org/licenses/lgpl-3.0.txt
+ * for more details.
+ */
+
 #include <gjs-commonjs-context.h>
 
 gint
